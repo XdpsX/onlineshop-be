b7b691379652855b96b178fa7c23c7abb7654ee5
Night <dranhata@gmail.com>
2024-06-16 23:03:41 +0700

    add max upload size handler and logging for each handler

--- server/src/main/java/com/xdpsx/ecommerce/exceptions/handler/GlobalExceptionHandler.java
+++ server/src/main/java/com/xdpsx/ecommerce/exceptions/handler/GlobalExceptionHandler.java
@@ -4,22 +4,36 @@ import com.xdpsx.ecommerce.dtos.error.ErrorDetails;
 import com.xdpsx.ecommerce.exceptions.BadRequestException;
 import com.xdpsx.ecommerce.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDetails handleMaxUploadSizeExceeded(HttpServletRequest request, MaxUploadSizeExceededException e) {
+        log.error(e.getMessage(), e);
+        ErrorDetails errorDetails = new ErrorDetails("File size limit exceeded. Please upload a file with a smaller size.");
+        errorDetails.setStatus(HttpStatus.BAD_REQUEST.value());
+        errorDetails.setPath(request.getServletPath());
+        return errorDetails;
+    }
 
     @ExceptionHandler(ResourceNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorDetails handleResourceNotFound(HttpServletRequest request, ResourceNotFoundException e) {
+        log.error(e.getMessage(), e);
         ErrorDetails errorDetails = new ErrorDetails(e.getMessage());
         errorDetails.setStatus(HttpStatus.NOT_FOUND.value());
         errorDetails.setPath(request.getServletPath());
@@ -29,6 +43,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(BadRequestException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorDetails handleBadRequest(HttpServletRequest request, BadRequestException e){
+        log.error(e.getMessage(), e);
         ErrorDetails errorDetails = new ErrorDetails(e.getMessage());
         errorDetails.setStatus(HttpStatus.BAD_REQUEST.value());
         errorDetails.setPath(request.getServletPath());
@@ -38,6 +53,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorDetails handleMethodArgumentNotValid(HttpServletRequest request, MethodArgumentNotValidException e) {
+        log.error(e.getMessage(), e);
         ErrorDetails errorDetails = new ErrorDetails("Validation Error");
         errorDetails.setStatus(HttpStatus.BAD_REQUEST.value());
         errorDetails.setPath(request.getServletPath());
@@ -51,8 +67,9 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorDetails handleException(HttpServletRequest request, Exception e) {
+        log.error(e.getMessage());
         ErrorDetails errorDetails = new ErrorDetails("Internal Server Error");
-        errorDetails.setStatus(HttpStatus.BAD_REQUEST.value());
+        errorDetails.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
         errorDetails.setPath(request.getServletPath());
         return errorDetails;
     }
