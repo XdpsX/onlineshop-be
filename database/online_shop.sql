2b8b93a13b3d95ef5ccc140c2450b7d985e876fe
Night <dranhata@gmail.com>
2024-08-28 20:26:06 +0700

    add brands and category_brands tables

--- database/online_shop.sql
+++ database/online_shop.sql
@@ -3,3 +3,17 @@ CREATE TABLE IF NOT EXISTS categories (
     name VARCHAR(128) NOT NULL UNIQUE,
     slug VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS brands (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(64) NOT NULL UNIQUE,
+    logo VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS category_brands (
+    brand_id INT,
+    category_id INT,
+    PRIMARY KEY (brand_id, category_id),
+    FOREIGN KEY (brand_id) REFERENCES brands(id) ON DELETE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE
+);
