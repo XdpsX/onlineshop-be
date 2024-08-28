b5ccf2268fdc0667c3ffa44856cbef964e39ead2
Night <dranhata@gmail.com>
2024-08-28 21:10:13 +0700

    add cloudinary config and test cloudinary connection

--- server/src/test/java/com/xdpsx/onlineshop/OnlineShopApplicationTests.java
+++ server/src/test/java/com/xdpsx/onlineshop/OnlineShopApplicationTests.java
@@ -1,13 +1,33 @@
 package com.xdpsx.onlineshop;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class OnlineShopApplicationTests {
+    @Autowired
+    private Cloudinary cloudinary;
 
-//	@Test
-//	void contextLoads() {
-//	}
+    @DisplayName("Test Cloudinary Connection")
+    @Test
+    public void testCloudinaryConnection() throws Exception {
+        // Kiểm tra xem Cloudinary bean có được khởi tạo không
+        assertNotNull(cloudinary);
+
+        // Thực hiện một yêu cầu đơn giản để kiểm tra kết nối
+        Map<String, Object> response = cloudinary.api().resources(ObjectUtils.asMap("max_results", 1));
+
+        // Kiểm tra xem phản hồi có hợp lệ không
+        assertNotNull(response);
+        System.out.println("Cloudinary connection successful: " + response);
+    }
 
 }
