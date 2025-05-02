a32e4ffa4885a5f5e3cd9a15880b26c2f859a1dc
XdpsX <dranhata@gmail.com>
2025-05-02 09:18:42 +0700

    feat: add scheduled tasks for cleaning up media (#24)

--- src/main/java/com/xdpsx/onlineshop/repositories/MediaRepository.java
+++ src/main/java/com/xdpsx/onlineshop/repositories/MediaRepository.java
@@ -1,7 +1,21 @@
 package com.xdpsx.onlineshop.repositories;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.xdpsx.onlineshop.entities.Media;
 
-public interface MediaRepository extends CrudRepository<Media, String> {}
+public interface MediaRepository extends CrudRepository<Media, String> {
+    @Query("SELECT m FROM Media m WHERE m.deleteFlg = true")
+    List<Media> findDeletedMedia();
+
+    @Query("""
+		SELECT m FROM Media m
+		WHERE m.tempFlg = true AND m.createdAt < :expiryTime
+		""")
+    List<Media> findExpiredMedia(@Param("expiryTime") LocalDateTime expiryTime);
+}
